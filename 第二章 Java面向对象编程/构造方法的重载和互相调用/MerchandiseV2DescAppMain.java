95c96fd20fa1522e80e08c3afae07195374e5908
LearningChanging <cool_kj@126.com>
2020-07-07 23:02:21 +0800

    构造方法的重载和互相调用

---
+++ 第二章 Java面向对象编程/构造方法的重载和互相调用/MerchandiseV2DescAppMain.java
@@ -0,0 +1,11 @@
+package 构造方法的重载和互相调用;
+
+import 构造方法的重载和互相调用.supermarket.MerchandiseV2;
+
+public class MerchandiseV2DescAppMain {
+    public static void main(String[] args) {
+        MerchandiseV2 merchandise = new MerchandiseV2();
+
+        merchandise.describe();
+    }
+}
