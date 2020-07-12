47eeccbf88e692c911bf7da030bbdf57a6f3f41a
LearningChanging <cool_kj@126.com>
2020-07-12 23:49:13 +0800

    自动生成toString

---
+++ 第二章 Java面向对象编程/toString方法/ToStringAppMain.java
@@ -0,0 +1,28 @@
+package toString方法;
+
+import toString方法.supermarket.LittleSuperMarket;
+import toString方法.supermarket.MerchandiseV2;
+
+public class ToStringAppMain {
+    public static void main(String[] args) {
+        LittleSuperMarket superMarket = new LittleSuperMarket("大卖场",
+                "世纪大道1号", 500, 600, 100);
+
+        MerchandiseV2 m100 = superMarket.getMerchandiseOf(100);
+
+        StringBuilder strBuilder = new StringBuilder();
+
+        strBuilder.append("商品100是：").append(m100);
+
+        // >> TODO 因为toString是Object里的方法，所以任何一个Java的对象，都可以调用这个方法
+        // >> TODO 内容好像不大全，补充一下？
+        System.out.println(strBuilder.toString());
+        System.out.println(m100);
+
+        String b = "b: ";
+        b += m100;
+        System.out.println(b);
+
+    }
+
+}
