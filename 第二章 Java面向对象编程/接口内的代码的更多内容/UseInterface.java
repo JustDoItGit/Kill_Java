9d75028e84985b32c12c45c3b1fd7800da639108
cool <cool_kj@126.com>
2020-07-14 16:51:06 +0800

    接口内的代码的更多内容

---
+++ 第二章 Java面向对象编程/接口内的代码的更多内容/UseInterface.java
@@ -0,0 +1,24 @@
+package 接口内的代码的更多内容;
+
+
+import 接口内的代码的更多内容.supermarket.*;
+
+import java.util.Date;
+
+public class UseInterface {
+
+    public static void main(String[] args) {
+
+        Date produceDate = new Date();
+        Date expireDate = new Date(produceDate.getTime() + 365L * 24 * 3600 * 1000);
+        GamePointCard gamePointCard = new GamePointCard(
+                "手机001", "Phone001", 100, 1999, 999,
+                produceDate, expireDate
+        );
+
+        // >> TODO 父类的引用可以用子类的引用赋值，抽象类也一样
+        ExpireDateMerchandise am = gamePointCard;
+
+
+    }
+}
