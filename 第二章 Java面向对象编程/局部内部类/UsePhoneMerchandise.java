95c95a139427e958b73a5b97c6ac7d58eed29ed9
LearningChanging <cool_kj@126.com>
2020-07-15 00:34:57 +0800

    局部内部类

---
+++ 第二章 Java面向对象编程/局部内部类/UsePhoneMerchandise.java
@@ -0,0 +1,17 @@
+package 局部内部类;
+
+
+import 局部内部类.supermarket.Phone;
+
+public class UsePhoneMerchandise {
+    public static void main(String[] args) {
+        Phone phone = new Phone(
+                "手机001", "Phone001", 100, 1999, 999,
+                4.5, 3.5, 4, 128, "索尼", "安卓"
+        );
+
+        // phoneExtendsMerchandise.describe();
+
+        phone.describePhone();
+    }
+}
