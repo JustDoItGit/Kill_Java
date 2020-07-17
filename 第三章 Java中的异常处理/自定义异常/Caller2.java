6435d37c1dc22cc909c03967a604fbad46c01745
cool <cool_kj@126.com>
2020-07-17 10:45:08 +0800

    自定义异常

---
+++ 第三章 Java中的异常处理/自定义异常/Caller2.java
@@ -0,0 +1,20 @@
+package 自定义异常;
+
+import 自定义异常.myexceptions.MyException;
+
+public class Caller2 {
+    Caller3 caller3 = new Caller3();
+
+    public void call3RTException() {
+        System.out.println("Caller2.call3RTException开始");
+        caller3.callThrowRTException();
+        System.out.println("Caller2.Caller2call3RTException结束");
+
+    }
+
+    public void call3Exception() throws MyException {
+        System.out.println("Caller2.call3Exception开始");
+        caller3.callThrowException();
+        System.out.println("Caller2.call3Exception结束");
+    }
+}
