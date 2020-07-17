60794fc4d903e2c0d19b74820b98553012e82d65
cool <cool_kj@126.com>
2020-07-17 16:27:07 +0800

    Collection类族简介

---
+++ 第四章 Java 中的常用工具类和新语法/Collection类族简介/WhatsCollectionAppMain.java
@@ -0,0 +1,9 @@
+package Collection类族简介;
+
+import java.util.Collection;
+
+public class WhatsCollectionAppMain {
+    public static void main(String[] args) {
+        Collection collection = null;
+    }
+}
