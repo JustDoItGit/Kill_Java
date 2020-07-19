36ade97c8312ec4f221a20b3f313d2eee28ecd28
LearningChanging <cool_kj@126.com>
2020-07-19 23:56:46 +0800

    多线程

---
+++ 第五章 Java 中的线程/多线程/DataHolder.java
@@ -0,0 +1,14 @@
+package 多线程;
+
+public class DataHolder {
+    private long number = 0;
+
+    public void change(long delta) {
+        number += delta;
+    }
+
+    public void print() {
+        System.out.println("Number=" + number);
+    }
+
+}
