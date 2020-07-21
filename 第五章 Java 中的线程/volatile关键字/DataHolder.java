0324c7310b58e13c648d74910694bff58e8d49ec
LearningChanging <cool_kj@126.com>
2020-07-21 23:06:53 +0800

    volatile 修饰就会影响之前的指令重排

--- 第五章 Java 中的线程/volatile关键字/DataHolder.java
+++ 第五章 Java 中的线程/volatile关键字/DataHolder.java
@@ -2,7 +2,9 @@ package volatile关键字;
 
 public class DataHolder {
     int a, b, c, d, f, g;
-    long e;
+    //    long e;
+    // TODO 有 volatile 修饰就会影响之前的指令重排
+    volatile long e;
 
     public void operateData() {
         // TODO 按照这个顺序执行，g 的值是肯定小于等于 e 的。但是实际执行在执行的时候，可能会为了优化的目的重排
