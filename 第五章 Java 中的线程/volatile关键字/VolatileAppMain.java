013bf0273f84afcacf8ad0ed57029176d3a21df6
LearningChanging <cool_kj@126.com>
2020-07-21 23:02:31 +0800

    演示指令重排引发的意外

---
+++ 第五章 Java 中的线程/volatile关键字/VolatileAppMain.java
@@ -0,0 +1,21 @@
+package volatile关键字;
+
+public class VolatileAppMain {
+    public static void main(String[] args) {
+        DataHolder dataHolder = new DataHolder();
+
+        Thread operator = new Thread(()->{
+            while (true) {
+                dataHolder.operateData();
+            }
+        });
+        operator.start();
+
+        Thread checker = new Thread(() -> {
+           while (true) {
+               dataHolder.check();
+           }
+        });
+        checker.start();
+    }
+}
